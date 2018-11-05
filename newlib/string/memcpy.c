7c4b360d2b7e7d439308600e070144781b531f42
Dennis Boon <dennisvdboon@gmail.com>
2018-11-05 16:14:30 +0000

    memcpy now checks for valid arguments.

--- newlib/string/memcpy.c
+++ newlib/string/memcpy.c
@@ -1,3 +1,9 @@
+/*
+ * $Id$
+ *
+ * Adapted for NewLib3 by Dennis van der Boon in 2018
+ */
+
 #pragma pack(2)
 #include <powerpc/powerpc_protos.h>
 #pragma pack()
@@ -6,5 +12,7 @@
 
 void *memcpy(void *s1,const void *s2,size_t n)
 {
+  if (s1 == NULL || s2 == NULL || n == 0)
+    return NULL;
   CopyMemPPC((APTR)s2,s1,n); return s1;
 }
